80d923bf48248ebf76b5b17b529999205fe06ef9
LucasMachuca <lucasamachuca07@gmail.com>
2026-09-15 01:35:31 -0300

    feat: implementar votacion y eliminacion de enlaces anidados con acciones en la UI

--- index.js
+++ index.js
@@ -4,25 +4,103 @@ const express = require ('express');
 // 2. Inicializamos la aplicacion
 const app = express ();
 
+// Le indicamos a Express que usaremos EJS como motor de plantillas 
+app.set ('view engine', 'ejs')
+
+// Permite que se pueda interpretar los datos enviados desde el html.
+app.use (express.urlencoded ({extended: true}));
+
 //3. Definimos el puerto de red
 const PORT = 3000;
 
-// Le indicamos a Express que usaremos EJS como motor de plantillas 
-app.set ('view engine', 'ejs')
+// Importamos el Modelo que contiene los datos y el metodo getAll ()
+const Topic = require ('./models/topic');
+
 
 // Configuramos la carpeta de archivos estaticos
 app.use (express.static ('public'));
 
 //4. Definimos nuestra primera ruta donde escuchara el servidor 
 app.get ('/', (req, res) => {
-    res.render ('index: ',
+
+    const topics = Topic.getAll ();
+
+    res.render ('index',
         {
             titulo: 'Bienvenido a Leran It, Love it',
-            mensaje: 'Plataforma colaborativa para aprender habilidades increibles'
+            mensaje: 'Plataforma colaborativa para aprender habilidades increibles',
+            topics: topics
         });
+});
+
+// Definimos la ruta donde enviara los datos el formulario al servidor 
+app.post ('/topics', (req, res) => {
+    // Extraemos los campos que el usuario escribio en el formato
+    const {title, description} = req.body;
+
+    // Le pedimos al modelo que cree el nuevo tema 
+    Topic.create ({title, description});
+
+    // Redirigimos al usuario a la pagina principal
+    res.redirect ('/');
+});
+
+// Ruta para poder votar los temas 
+app.post ('/topics/:id/vote', (req, res) => {
+
+    // Extraemos el id dinamico desde los parametros de la URL
+    const {id} = req.params;
+
+    // Le pedimos al modelo que incremente el contador de votos 
+    Topic.upvotes (id);
+
+    // Redirigimos a la pagina principal para refrescar la lista y el orden
+    res.redirect ('/');
+
+});
+
+// Ruta para eliminar un tema especifico 
+app.post ('/topics/:id/delete', (req, res) => {
+    const topicID = req.params.id;
+
+    const wasDelete = Topic.delete (topicID);
+
+    if (!wasDelete){
+        console.warn (
+            `Intendo de eliminar un tema inexistente con ID: ${topicID}`
+        )
+    }
+
+    //Redirigimos al inicio para ver la lista fresca
+    res.redirect ('/');
+});
+
+//Ruta para poder aumentar el voto de un link asociado a un tema 
+app.post ('/topics/:topicID/links/:linkID/vote', (req, res) => {
+    const {topicID, linkID} = req.params;
+
+    const updateLink = Topic.upvoteLink (topicID, linkID);
+
+    if (!updateLink) {
+        console.warn (`No se pudo votar: Tema ${topicID} o link ${linkID} no encontrados`);
+    }
+
+    res.redirect ('/');
+});
+
+app.post ('/topics/:topicID/links/:linkID/delete', (req, res) => {
+    const {topicID, linkID} = req.params;
+
+    const delLink = Topic.deleteLink (topicID, linkID);
+
+    if (!delLink){
+        console.warn (`No se pudo elimianr: Tema ${topicID} o link ${linkID} no encontrados`);
+    }
+
+    res.redirect ('/');
 });
 
 //5. Ponemos el servidor a escuchar peticiones
 app.listen (PORT, () => {
-    console.log ('Servidor activo y escuchando en http://localhost:${PORT}');
+    console.log (`Servidor activo y escuchando en http://localhost:${PORT}`);
 });
---
+++ models/topic.js
@@ -0,0 +1,98 @@
+// Estructura de datos en memoria paa almacenar los tmeas 
+let topics = [
+    {
+        id:1,
+        title:"Como progamar como un ninja",
+        description:"Aprende los fundamentos de JavaScript y Node.js desde cero.",
+        votes: 5,
+        links:[
+            {
+                id:101,
+                title: "Documento oficial de Nodes.js",
+                votes: 2
+            }
+        ]
+    },
+    {
+        id:2,
+        title: "Dominar el arte de prepar cafe",
+        description: "Guia pactica para el preparado de cafe ",
+        votes: 3,
+        links: []
+    }
+];
+
+class Topic {
+    //Retorna todos los temas ordenados de mayor a menor segun los votos 
+    static getAll (){
+        return [...topics].sort ((item_a, item_b)=> item_b.votes - item_a.votes);
+    }
+    
+    //Metodo donde creamos los temas 
+    static create ({title, description}){
+        const newTopic = {
+            id:Date.now (),
+            title,
+            description,
+            votes: 0,
+            links: []
+        };
+
+        topics.push (newTopic);
+        return newTopic;
+    }
+
+    //Metodo que aumenta el voto 
+    static upvotes (id){
+        const topic = topics.find (t => t.id === Number (id));
+        if (topic){
+            topic.votes +=1;
+            return topic;
+        }
+        return null;
+    }
+
+    //Metodo que elimina un tema mediante el id recibido
+    static delete (id){
+        const index = topics.findIndex (t => t.id === Number (id));
+        if (index == -1){
+            return false; //El tema no existe 
+        }
+        topics.splice (index, 1); //Quita un elemento en esa posicion
+        return true;
+    }
+
+    //Se encarga de aumentar los votos asociados a un tema
+    static upvoteLink (topicID, linkID){
+        const topic = topics.find (t => t.id === Number(topicID));
+        if (!topic || !topic.links){
+            return null;
+        }
+
+        const link = topic.links.find (l => l.id ===Number (linkID));
+        if (!link){
+            return null;
+        }
+        link.votes +=1;
+        return link;
+    }
+
+    // Se encarga de eliminar los links asociados a un tema 
+    static deleteLink (topicID, linkID){
+        const topic = topics.find (t => t.id === Number (topicID));
+        if (!topic || !topic.links ){
+            return false;
+        }
+
+        const linkIndex = topic.links.findIndex (l => l.id === Number (linkID));
+        if (linkIndex === -1){
+            return false;
+        }
+        topic.links.splice (linkIndex, 1);
+        return true;
+    }
+}
+
+
+// Exportamos la clase para que pueda ser utilizada en los controladores
+module.exports = Topic;
